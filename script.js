903ad5f8606fd5777b7af4dd01db08ad9135dedc
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2022-11-04 16:00:37 -0300

    improvements

--- script.js
+++ script.js
@@ -11,40 +11,44 @@ function calc() {
     let cValue = child.value;
     let hValue = hours.value;
 
+    if (!aValue || !cValue || !hValue) {
+        res.innerHTML = `<p><span class="titleJava">Todos os campos devem ser preenchidos!</span></p>`
+    } else {
 
-    function calcMeat() {
-
-        if (hValue < 6) {
-            return (aValue * 500) + (cValue * 250);
-        } else {
-            return (aValue * 750) + (cValue * 375);
+        function calcMeat() {
+            if (hValue < 6) {
+                return (aValue * 500) + (cValue * 250);
+            } else {
+                return (aValue * 750) + (cValue * 375);
+            }
         }
-    }
 
-    function calcBeer() {
-        if (hValue < 6) {
-            return (aValue * 1800);
-        } else {
-            return (aValue * 3600);
+        function calcBeer() {
+            if (hValue < 6) {
+                return (aValue * 1800);
+            } else {
+                return (aValue * 3600);
+            }
         }
-    }
 
-    function calcDrink() {
-        if (hValue < 6) {
-            return (aValue * 600) + (cValue * 300);
-        } else {
-            return (aValue * 800) + (cValue * 400);
+        function calcDrink() {
+            if (hValue < 6) {
+                return (aValue * 600) + (cValue * 500);
+            } else {
+                return (aValue * 800) + (cValue * 700);
+            }
         }
+
+        res.innerHTML = `<p><span class="titleJava">Você precisará de:</span></p><br>`
+
+        res.innerHTML += `<p><i class="fa-solid fa-drumstick-bite fa-xl"></i>
+    ${calcMeat() / 1000}Kg de <span class="textJava">carne</span>.</p><br>`
+
+        res.innerHTML += `<p><i class="fa-solid fa-beer-mug-empty fa-xl"></i>
+    ${calcBeer() / 1000}L de <span class="textJava">cerveja</span>.</p><br>`
+
+        res.innerHTML += `<p><i class="fa-solid fa-glass-water fa-xl"></i>
+    ${calcDrink() / 1000}L de <span class="textJava">água/refrigerante</span>.</p><br>`
+
     }
-
-    res.innerHTML = `<p><span class="titleJava">Você precisará de:</span></p><br>`
-
-    res.innerHTML += `<p><img src="./assets/meat.png" class="fotoJava">
-    ${calcMeat()/1000}Kg de <span class="textJava">carne</span>.</p><br>`
-
-    res.innerHTML += `<p><img src="./assets/beer.png" class="fotoJava">
-    ${calcBeer()/1000}L de <span class="textJava">cerveja</span>.</p><br>`
-
-    res.innerHTML += `<p><img src="./assets/sodawater.png" class="fotoJava">
-    ${calcDrink()/1000}L de <span class="textJava">água/refrigerante</span>.</p><br>`
 }
